566120279ada526b612fd48315d8c7b0f0a0d1cf
chuanwise <chuanwise@qq.com>
2024-01-03 15:58:20 +0800

    build(nut-core): init cmake directories

---
+++ nut-core/src/test.cpp
@@ -0,0 +1,12 @@
+#include <nut/core/test>
+#include <iostream>
+
+namespace nut {
+  namespace core {
+    namespace test {
+      void hello_world() {
+        std::cout << "Hello World! " << std::endl;
+      }
+    }
+  }
+}
---
+++ nut-core/tests/test-hello-world.cpp
@@ -0,0 +1,5 @@
+#include <nut/core/test>
+
+int main() {
+  nut::core::test::hello_world();
+}
